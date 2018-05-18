12357c03a6a89442bfe647cacf87b24f73c776cb
Judy Park <jparkmhg@gmail.com>
2018-05-18 06:42:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name)(string) {
-  if (string === $(name)) {
+  if (string === ${name}) {
     return `Happy holidays, ${name}!`
   }
 }
